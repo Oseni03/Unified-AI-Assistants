120f553ad9d495f5067724bfa81cf6fdfdd24f3f
Oseni03 <oseniayomide57@gmail.com>
2024-04-30 14:06:13 +0100

    modify settings for venv file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -7,7 +7,7 @@ from urllib.parse import urljoin
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".venv"))
 
 env = environ.Env(
     # set casting, default value
