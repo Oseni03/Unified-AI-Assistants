7e6e6ed8460cc633065fb39e4b0d28f2067c4dd1
Oseni03 <oseniayomide57@gmail.com>
2024-06-04 12:38:27 +0100

    add feedback to agent view

--- backend/agents/views.py
+++ backend/agents/views.py
@@ -1,9 +1,13 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import mixins, permissions, status
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from integrations.models import Agent
+from feedbacks.serializers import FeedBackSerializer
+from feedbacks.models import FeedBack
 
 from .serializers import AgentSerializer, MessageSerializer
 from .models import Message
@@ -30,11 +34,29 @@ class AgentViewSet(mixins.RetrieveModelMixin,
         if request.method == "GET":
             agent = self.get_object()
             messages = Message.objects.filter(agent=agent)
-            serializers = MessageSerializer(messages, many=True)
-            return Response(serializers.data, status=status.HTTP_200_OK)
+            serializer = MessageSerializer(messages, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         elif request.method == "POST":
             agent = self.get_object()
             serializer = MessageSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(agent=agent)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=True, methods=["get", "post"], serializer_class=FeedBackSerializer)
+    def feedbacks(self, request, pk=None):
+        agent = self.get_object()
+        if request.method == "GET":
+            if pk:
+                feedback = get_object_or_404(FeedBack, id=pk)
+                serializer = FeedBackSerializer(feedback, many=False)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                feedbacks = FeedBack.objects.filter(agent=agent)
+                serializer = FeedBackSerializer(feedbacks, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        elif request.method == "POST":
+            serializer = FeedBackSerializer(data=request.data)
             if serializer.is_valid(raise_exception=True):
                 serializer.save(agent=agent)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
